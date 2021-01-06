aba347af0948fca88f5c2e6b6b09b16873d64751
SANDEEP <76508612+sandeepfullstackacademy@users.noreply.github.com>
2021-01-06 23:23:43 +0530

    Add files via upload

---
+++ ascending.js
@@ -0,0 +1,14 @@
+var numbers = [23,25,400]
+
+{
+    console.log("numbers in ascending order");
+}
+else
+{
+    var sortednumbers = numbers.sort((a,b)=> a -b);
+{
+    console.log("given numbers are not display in ascending order");
+}
+    
+}
+
